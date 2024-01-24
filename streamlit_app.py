cd70fd7bb5b5665b0526401cd8b39205132f489a
RolandSQLDev <157634836+RolandSQLDev@users.noreply.github.com>
2024-01-24 15:48:21 -0300

    Update streamlit_app.py
    I Add items of menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents New Headthy Diner')
 streamlit.title('Breakfast Menu')
+streamlit.title('Omega 3 & Blueberry Oatmeal')
+streamlit.title('Male, Spinach & Rocket Smoothie')
+streamlit.title('Hard-Boiled Free-Range Egg')
