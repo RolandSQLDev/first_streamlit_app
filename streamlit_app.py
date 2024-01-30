758dae94d9bdecb60870297e9d37453c06253ce9
RolandSQLDev <157634836+RolandSQLDev@users.noreply.github.com>
2024-01-30 18:47:12 -0300

    Update streamlit_app.py
    Convert the Old Imports to Comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit
-import pandas
+#import pandas
+#import requests
+import snowflake.connector
+from urllib.error 
+import URLError
+
 
 streamlit.title('My parents New Headthy Diner')
 streamlit.header('Breakfast Menu')
@@ -24,7 +29,6 @@ streamlit.dataframe(fruits_to_show)
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
@@ -37,7 +41,7 @@ streamlit.dataframe(fruityvice_normalized)
 # don't run anything past here while we trouble shoot 
 snowflake.stop()
 
-import snowflake.connector
+
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
